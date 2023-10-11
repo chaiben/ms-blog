21d1f2519bb526162e4eb7cd2fd4abc7d259cccf
Marçal Machado Chaiben <chaiben79@gmail.com>
2023-10-11 07:18:46 +0200

    Inital commit

---
+++ blog/comments/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const { id: postId } = req.params;
+
+  const comments = commentsByPostId[postId] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[postId] = comments;
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  res.status(201).send(commentsByPostId[postId]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
