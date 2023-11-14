176f8075c14c9cbceb666488d9721baafa12fc54
Marçal Machado Chaiben <chaiben79@gmail.com>
2023-11-14 08:22:36 +0100

    Created all deployment services

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -21,7 +21,7 @@ app.post("/posts", async (req, res) => {
   posts[id] = { id, title };
 
   await axios
-    .post("http://localhost:4005/events", {
+    .post("http://event-bus-srv:4005/events", {
       type: "PostCreated",
       data: posts[id],
     })
